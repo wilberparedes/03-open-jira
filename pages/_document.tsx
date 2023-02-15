2783ba3d4acb49c4b5da8271c989a21f742b77eb
Wilber Paredes <wilberparedes@gmail.com>
2023-02-15 18:32:34 -0500

    90. Instalación de Material-UI

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <link
+          rel='stylesheet'
+          href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
+        />
+        <link rel='shortcut icon' href='/favicon.ico' />
+      </Head>
       <body>
         <Main />
         <NextScript />
