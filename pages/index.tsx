adc282668abb5312ac5e7dddfb78fc17d19003e4
Wilber Paredes <wilberparedes@gmail.com>
2023-02-20 17:23:48 -0500

    106. Diseño de la pantalla principal - index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,33 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import { Typography } from '@mui/material'
+import { NextPage } from 'next'
+
+import { Card, CardContent, CardHeader, Grid } from '@mui/material'
+
 import { Layout } from '@/components/layouts'
 
-const inter = Inter({ subsets: ['latin'] })
-
-const HomePage = () => {
+const HomePage: NextPage = () => {
   return (
-    <Layout title='Home Page'>
-      <Typography variant='h1' color={'primary'}>
-        Hola Mundo
-      </Typography>
+    <Layout title='Home - OpenJira'>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: 'calc(100vh - 64px)' }}>
+            <CardHeader title='Pendientes' />
+            <CardContent>
+              {/* Agregar una nueva entra */}
+              {/* Listado de las entradas */}
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: 'calc(100vh - 64px)' }}>
+            <CardHeader title='En Progreso' />
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: 'calc(100vh - 64px)' }}>
+            <CardHeader title='Completadas' />
+          </Card>
+        </Grid>
+      </Grid>
     </Layout>
   )
 }
