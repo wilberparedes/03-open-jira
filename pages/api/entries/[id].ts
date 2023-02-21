c853008312772d91caec420d325dbda1c5fb9eb6
Wilber Paredes <wilberparedes@gmail.com>
2023-02-21 01:55:46 -0500

    134. Actualizar una entrada en particular - Backend

---
+++ pages/api/entries/[id].ts
@@ -0,0 +1,59 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import mongoose from 'mongoose'
+
+import { db } from '@/database'
+import { EntryModel, IEntry } from '@/models'
+
+type Data =
+  | {
+      message: string
+    }
+  | IEntry
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const { id } = req.query
+
+  if (!mongoose.isValidObjectId(id)) {
+    return res.status(400).json({ message: `El id no es válido ${id}` })
+  }
+
+  switch (req.method) {
+    case 'PUT':
+      return updateEntry(req, res)
+
+    default:
+      return res.status(400).json({ message: 'Método no existe' })
+  }
+}
+
+const updateEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const { id } = req.query
+
+  await db.connect()
+
+  const entryToUpdate = await EntryModel.findById(id)
+  if (!entryToUpdate) {
+    await db.disconnect()
+    return res.status(400).json({ message: `No hay entrada con ese ID: ${id}` })
+  }
+
+  const {
+    description = entryToUpdate.description,
+    status = entryToUpdate.status,
+  } = req.body
+
+  const updatedEntry = await EntryModel.findByIdAndUpdate(
+    id,
+    {
+      description,
+      status,
+    },
+    { runValidators: true, new: true }
+  )
+
+  res.status(200).json(updatedEntry!)
+}
