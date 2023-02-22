ef7e517514e2ac26ecc545d021a83fefb90bc6a4
Wilber Paredes <wilberparedes@gmail.com>
2023-02-22 16:25:04 -0500

    140. Middleware - Verificar que sea un MongoID

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,38 @@
-// middleware.ts
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-// This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  console.log(request.nextUrl)
+export function middleware(req: NextRequest) {
+  if (req.nextUrl.pathname.startsWith('/api/entries/')) {
+    const id = req.nextUrl.pathname.replace('/api/entries/', '')
+    console.log({ id })
+    const checkMongoIDRegExp = new RegExp('^[0-9a-fA-F]{24}$')
+    if (!checkMongoIDRegExp.test(id)) {
+      return new NextResponse(
+        JSON.stringify({
+          success: false,
+          message: `${id} is not a valid MongoID`,
+        }),
+        { status: 400, headers: { 'content-type': 'application/json' } }
+      )
+      /**
+       * const url = req.nextUrl.clone()
+       * url.pathname = '/api/bad-request'
+       * url.search = `?message=${id} is not a valid MongoID`
+       * return NextResponse.rewrite(url)
+       */
+      /**
+       * return NextResponse.redirect(
+       * new URL(
+       * `/api/bad-request?message=${id} is not a valid MongoID`,
+       * req.url
+       * )
+       * )
+       */
+    }
+  }
   return NextResponse.next()
 }
 
-// See "Matching Paths" below to learn more
 export const config = {
-  //   matcher: '/about/:path*',
-  matcher: ['/api/entries/:path'],
+  matcher: ['/api/entries/:path*'],
 }
