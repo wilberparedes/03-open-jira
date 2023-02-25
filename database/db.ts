9d24c0ea1588c7706f979e9e4fc771568d2b54d4
Wilber Paredes <wilberparedes@gmail.com>
2023-02-25 15:10:47 -0500

    fix: strictquery to mongoose

--- database/db.ts
+++ database/db.ts
@@ -13,6 +13,7 @@ const mongoConnection = {
 
 export const connect = async () => {
   if (mongoConnection.isConnected) {
+    // eslint-disable-next-line no-console
     console.log('Ya estabamos conectados')
     return
   }
@@ -20,13 +21,16 @@ export const connect = async () => {
   if (mongoose.connections.length) {
     mongoConnection.isConnected = mongoose.connections[0].readyState
     if (mongoConnection.isConnected === 1) {
+      // eslint-disable-next-line no-console
       console.log('Usando conexión anterior')
       return
     }
     await mongoose.disconnect()
   }
+  mongoose.set('strictQuery', false)
   await mongoose.connect(process.env.MONGO_URL || '')
   mongoConnection.isConnected = 1
+  // eslint-disable-next-line no-console
   console.log('Conectado a MongoDB: ', process.env.MONGO_URL)
 }
 
@@ -41,5 +45,6 @@ export const disconnect = async () => {
   await mongoose.disconnect()
   mongoConnection.isConnected = 0
 
+  // eslint-disable-next-line no-console
   console.log('Desconectado de MongoDB')
 }
